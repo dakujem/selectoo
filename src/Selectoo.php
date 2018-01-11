b12c78abd648e2bb24b297cdc3b21e9dde607234
Andrej Rypo <xrypak@gmail.com>
2018-01-11 20:04:08 +0100

    (cosmetic) moved methods around to group them semantically

--- src/Selectoo.php
+++ src/Selectoo.php
@@ -95,20 +95,16 @@ class Selectoo extends BaseControl
 
 
 	/**
-	 * Returns selected key or keys, depending.
+	 * Is the Selectoo input in multi-choice mode?
 	 *
-	 * @return string|int|array returns array in multi-choice mode, int or string in single-choice mode
+	 * In multi-choice mode,  array  values are expected and returned.
+	 * In single-choice mode, scalar values are expected and returned.
+	 *
+	 * @return bool true when in multi-choice mode
 	 */
-	public function getValue()
+	public function isMulti(): bool
 	{
-		$items = $this->getItems();
-		$disabled = $this->disabled;
-		if ($this->isMulti()) {
-			$val = array_values(array_intersect($this->value, array_keys($items)));
-			return is_array($disabled) ? array_diff($val, array_keys($disabled)) : $val;
-		}
-		$val = array_key_exists($this->value, $items) ? $this->value : null;
-		return isset($disabled[$val]) ? NULL : $val;
+		return $this->multiChoice;
 	}
 
 
@@ -148,33 +144,33 @@ class Selectoo extends BaseControl
 
 
 	/**
-	 * Validates that every selected option (value) is in the array of possible options (items).
+	 * Returns selected key or keys, depending.
 	 *
-	 * @param int|string|array $value
-	 * @return self
-	 * @throws InvalidArgumentException
+	 * @return string|int|array returns array in multi-choice mode, int or string in single-choice mode
 	 */
-	protected function validateValueInOptions($value)
+	public function getValue()
 	{
 		$items = $this->getItems();
+		$disabled = $this->disabled;
 		if ($this->isMulti()) {
-			$diff = array_diff($value, array_keys($items));
-			if ($diff) {
-				$set = Strings::truncate(implode(', ', array_map(function ($s) {
-											return var_export($s, true);
-										}, array_keys($items))), 70, '...');
-				$vals = (count($diff) > 1 ? 's' : '') . " '" . implode("', '", $diff) . "'";
-				throw new InvalidArgumentException("Value$vals are out of allowed set [$set] in field '{$this->name}'.");
-			}
-		} else {
-			if ($value !== null && !array_key_exists((string) $value, $items)) {
-				$set = Strings::truncate(implode(', ', array_map(function ($s) {
-											return var_export($s, true);
-										}, array_keys($items))), 70, '...');
-				throw new InvalidArgumentException("Value '$value' is out of allowed set [$set] in field '{$this->name}'.");
-			}
+			$val = array_values(array_intersect($this->value, array_keys($items)));
+			return is_array($disabled) ? array_diff($val, array_keys($disabled)) : $val;
 		}
-		return $this;
+		$val = array_key_exists($this->value, $items) ? $this->value : null;
+		return isset($disabled[$val]) ? NULL : $val;
+	}
+
+
+	/**
+	 * Returns selected key (not checked).
+	 *
+	 * Note that the result of this call is NOT SAFE to use because the input can have ANY raw value!
+	 *
+	 * @return string|int|array returns array in multi-choice mode, int or string on single-choice mode
+	 */
+	public function getRawValue()
+	{
+		return $this->value;
 	}
 
 
@@ -232,54 +228,16 @@ class Selectoo extends BaseControl
 
 
 	/**
-	 * Have the items/elements been loaded?
+	 * Set item callback.
+	 * The callback will be used to retrieve items when needed.
 	 *
-	 * @return bool
-	 */
-	protected function isLoaded(): bool
-	{
-		return $this->items !== null;
-	}
-
-
-	/**
-	 * Unload dynamically loaded items. Use this method to force reloading the items.
-	 *
-	 * Calling this method only makes sense when item callback is set. Has no effect otherwise.
-	 *
+	 * @param callable|null $itemCallback
 	 * @return self
 	 */
-	public function unload()
+	public function setItemCallback(callable $itemCallback = null)
 	{
-		if ($this->getItemCallback() !== null) {
-			$this->items = null;
-		}
+		$this->itemCallback = $itemCallback;
 		return $this;
-	}
-
-
-	/**
-	 * Load dynamic items and elements.
-	 * Note that when no callback is set, it resets items to empty array.
-	 *
-	 * @return self
-	 */
-	protected function reload()
-	{
-		$callable = $this->getItemCallback();
-		$this->setItems($callable !== null ? call_user_func($callable, $this->getRawValue(), $this) : []);
-		return $this;
-	}
-
-
-	/**
-	 * Will the items be loaded later when needed?
-	 *
-	 * @return bool
-	 */
-	protected function isDormant(): bool
-	{
-		return !$this->isLoaded() && $this->getItemCallback() !== null;
 	}
 
 
@@ -291,20 +249,6 @@ class Selectoo extends BaseControl
 	public function getItemCallback()
 	{
 		return $this->itemCallback;
-	}
-
-
-	/**
-	 * Set item callback.
-	 * The callback will be used to retrieve items when needed.
-	 *
-	 * @param callable|null $itemCallback
-	 * @return self
-	 */
-	public function setItemCallback(callable $itemCallback = null)
-	{
-		$this->itemCallback = $itemCallback;
-		return $this;
 	}
 
 
@@ -356,64 +300,6 @@ class Selectoo extends BaseControl
 
 
 	/**
-	 * Returns selected key (not checked).
-	 *
-	 * Note that the result of this call is NOT SAFE to use because the input can have ANY raw value!
-	 *
-	 * @return string|int|array returns array in multi-choice mode, int or string on single-choice mode
-	 */
-	public function getRawValue()
-	{
-		return $this->value;
-	}
-
-
-	/**
-	 * Loads HTTP data.
-	 *
-	 * @return void
-	 */
-	public function loadHttpData()
-	{
-		if ($this->isMulti()) {
-			$this->value = array_keys(array_flip($this->getHttpData(Form::DATA_TEXT)));
-		} else {
-			$raw = $this->getHttpData(Form::DATA_TEXT);
-			if ($raw !== null) {
-				$this->value = key(array($raw => null)); // this lousy trick converts numbers to integer, other values to string
-			}
-		}
-	}
-
-
-	/**
-	 * Set flag whether to validate the value when calling setValue.
-	 * When setting a value that is not within the possible options an exception will be thrown.
-	 *
-	 * Note that validating during setValue calls has the side effect of loading items.
-	 * That may cause problems when using dependent inputs because the other inputs might not be loaded yet.
-	 *
-	 * @param bool $validate
-	 * @return self
-	 */
-	public function validateValuesOnSet(bool $validate = true)
-	{
-		$this->validateValuesOnSet = $validate;
-		return $this;
-	}
-
-
-	/**
-	 * @return self
-	 */
-	public function addOptionAttributes(array $attributes)
-	{
-		$this->optionAttributes = $attributes + $this->optionAttributes;
-		return $this;
-	}
-
-
-	/**
 	 * Sets first prompt item in select box.
 	 *
 	 * @param  string|object
@@ -434,20 +320,6 @@ class Selectoo extends BaseControl
 	public function getPrompt()
 	{
 		return $this->prompt;
-	}
-
-
-	/**
-	 * Is the Selectoo input in multi-choice mode?
-	 *
-	 * In multi-choice mode,  array  values are expected and returned.
-	 * In single-choice mode, scalar values are expected and returned.
-	 *
-	 * @return bool true when in multi-choice mode
-	 */
-	public function isMulti(): bool
-	{
-		return $this->multiChoice;
 	}
 
 
@@ -475,6 +347,16 @@ class Selectoo extends BaseControl
 		}
 		parent::setDisabled(false);
 		$this->disabled = array_fill_keys($value, true);
+		return $this;
+	}
+
+
+	/**
+	 * @return self
+	 */
+	public function addOptionAttributes(array $attributes)
+	{
+		$this->optionAttributes = $attributes + $this->optionAttributes;
 		return $this;
 	}
 
@@ -579,17 +461,6 @@ class Selectoo extends BaseControl
 
 
 	/**
-	 * Return assigned script management routine.
-	 *
-	 * @return callable|null
-	 */
-	public function getScriptManagement()
-	{
-		return $this->scriptManagement;
-	}
-
-
-	/**
 	 * Set a script management routine.
 	 *
 	 * The routine is called after the control's UI script has been generated and can alter the script in any way.
@@ -606,6 +477,135 @@ class Selectoo extends BaseControl
 	{
 		$this->scriptManagement = $routine;
 		return $this;
+	}
+
+
+	/**
+	 * Return assigned script management routine.
+	 *
+	 * @return callable|null
+	 */
+	public function getScriptManagement()
+	{
+		return $this->scriptManagement;
+	}
+
+
+	/**
+	 * Set flag whether to validate the value when calling setValue.
+	 * When setting a value that is not within the possible options an exception will be thrown.
+	 *
+	 * Note that validating during setValue calls has the side effect of loading items.
+	 * That may cause problems when using dependent inputs because the other inputs might not be loaded yet.
+	 *
+	 * @param bool $validate
+	 * @return self
+	 */
+	public function validateValuesOnSet(bool $validate = true)
+	{
+		$this->validateValuesOnSet = $validate;
+		return $this;
+	}
+
+
+	/**
+	 * Validates that every selected option (value) is in the array of possible options (items).
+	 *
+	 * @param int|string|array $value
+	 * @return self
+	 * @throws InvalidArgumentException
+	 */
+	protected function validateValueInOptions($value)
+	{
+		$items = $this->getItems();
+		if ($this->isMulti()) {
+			$diff = array_diff($value, array_keys($items));
+			if ($diff) {
+				$set = Strings::truncate(implode(', ', array_map(function ($s) {
+											return var_export($s, true);
+										}, array_keys($items))), 70, '...');
+				$vals = (count($diff) > 1 ? 's' : '') . " '" . implode("', '", $diff) . "'";
+				throw new InvalidArgumentException("Value$vals are out of allowed set [$set] in field '{$this->name}'.");
+			}
+		} else {
+			if ($value !== null && !array_key_exists((string) $value, $items)) {
+				$set = Strings::truncate(implode(', ', array_map(function ($s) {
+											return var_export($s, true);
+										}, array_keys($items))), 70, '...');
+				throw new InvalidArgumentException("Value '$value' is out of allowed set [$set] in field '{$this->name}'.");
+			}
+		}
+		return $this;
+	}
+
+
+	/**
+	 * Have the items/elements been loaded?
+	 *
+	 * @return bool
+	 */
+	protected function isLoaded(): bool
+	{
+		return $this->items !== null;
+	}
+
+
+	/**
+	 * Unload dynamically loaded items. Use this method to force reloading the items.
+	 *
+	 * Calling this method only makes sense when item callback is set. Has no effect otherwise.
+	 *
+	 * @return self
+	 */
+	public function unload()
+	{
+		if ($this->getItemCallback() !== null) {
+			$this->items = null;
+		}
+		return $this;
+	}
+
+
+	/**
+	 * Load dynamic items and elements.
+	 * Note that when no callback is set, it resets items to empty array.
+	 *
+	 * @return self
+	 */
+	protected function reload()
+	{
+		$callable = $this->getItemCallback();
+		$this->setItems($callable !== null ? call_user_func($callable, $this->getRawValue(), $this) : []);
+		return $this;
+	}
+
+
+	/**
+	 * Will the items be loaded later when needed?
+	 *
+	 * @return bool
+	 */
+	protected function isDormant(): bool
+	{
+		return !$this->isLoaded() && $this->getItemCallback() !== null;
+	}
+
+
+	/**
+	 * Loads HTTP data.
+	 *
+	 * @return void
+	 */
+	public function loadHttpData()
+	{
+		if ($this->isMulti()) {
+			$this->value = array_keys(array_flip($this->getHttpData(Form::DATA_TEXT)));
+		} else {
+			$raw = $this->getHttpData(Form::DATA_TEXT);
+			if ($raw !== null) {
+				$this->value = key(array($raw => null)); // this lousy trick converts numbers to integer, other values to string
+			}
+		}
 	}
 
 
